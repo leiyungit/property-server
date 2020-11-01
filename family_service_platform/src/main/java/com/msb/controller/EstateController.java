4165ffec424621617946f6698f42e927977524db
yunubuntu <744561873@qq.com>
2020-11-02 00:20:19 +0800

    保存楼宇数据，及批量提交

--- family_service_platform/src/main/java/com/msb/controller/EstateController.java
+++ family_service_platform/src/main/java/com/msb/controller/EstateController.java
@@ -1,14 +1,20 @@
 package com.msb.controller;
 
 import com.alibaba.fastjson.JSON;
+import com.fasterxml.jackson.annotation.JsonAlias;
+import com.msb.bean.FcBuilding;
 import com.msb.bean.FcEstate;
 import com.msb.resultJson.ResultObject;
 import com.msb.service.EstateService;
+import com.sun.scenario.effect.impl.sw.sse.SSEBlend_SRC_OUTPeer;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 @RestController
@@ -27,18 +33,47 @@ public class EstateController {
     @RequestMapping("/insertEstate") // @RequestBody Map<String,Object> fcBuilding
     public String insertEstate(FcEstate fcEstate){
         System.out.println("insert estate ");
-        //System.out.println(request);
         System.out.println(fcEstate);
-        return JSON.toJSONString(new ResultObject("房产添加成功",1));
-      /*  Integer result = service.insertEstate(fcEstate);
+        //return JSON.toJSONString(new ResultObject("房产添加成功",1));
+        Integer result = service.insertEstate(fcEstate);
         ResultObject ro;
         if(result == 0){
             ro = new ResultObject("房产编码已存在，操作失败",0);
         }else{
             ro = new ResultObject("房产添加成功",1);
         }
-        return JSON.toJSONString(ro);*/
+        return JSON.toJSONString(ro);
     }
 
+    /**
+     * 添加住宅信息第二步，传入楼宇数量，页面初始化时新生成楼宇行记录，并返回
+     * @param buildingNumber
+     * @param estateCode
+     * @return
+     */
+    @RequestMapping("/selectBuilding")
+    public String selectBuilding(Integer buildingNumber,  String estateCode){
+        System.out.println("estate selectBuilding");
+        List<FcBuilding> fcBuildings = service.selectBuilding(buildingNumber, estateCode);
+        System.out.println(fcBuildings);
+        return JSON.toJSONString(new ResultObject(fcBuildings));
+    }
+
+    @RequestMapping("/updateBuilding")
+    public String updateBuilding(FcBuilding fcBuilding){
+        System.out.println(fcBuilding);
+        return JSON.toJSONString(new ResultObject(service.updateBuilding(fcBuilding)));
+    }
+
+    @RequestMapping("/updateBatchBuilding")
+    public String updateBatchBuilding(@RequestBody FcBuilding[] fcBuildings){
+        System.out.println("updateBatchBuilding ... ");
+        // System.out.println(fcBuildings);
+        for (FcBuilding fcBuilding : fcBuildings) {
+            System.out.println(fcBuilding);
+        }
+        return JSON.toJSONString(new ResultObject(0));
+        //return JSON.toJSONString(new ResultObject(service.updateBatchBuilding(fcBuildings)));
+    }
 
 }
--- family_service_platform/src/main/java/com/msb/service/EstateService.java
+++ family_service_platform/src/main/java/com/msb/service/EstateService.java
@@ -1,13 +1,16 @@
 package com.msb.service;
 
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.msb.bean.FcBuilding;
 import com.msb.bean.FcEstate;
 import com.msb.bean.TblCompany;
+import com.msb.mapper.FcBuildingMapper;
 import com.msb.mapper.FcEstateMapper;
 import com.msb.mapper.TblCompanyMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Service
@@ -18,6 +21,9 @@ public class EstateService {
 
     @Autowired
     private FcEstateMapper fcEstateMapper;
+
+    @Autowired
+    private FcBuildingMapper fcBuildingMapper;
 
     public List<TblCompany> findCompay(){
         return tblCompanyMapper.findCompany();
@@ -34,5 +40,31 @@ public class EstateService {
             return result;
         }
         return result;
+    }
+
+    public List<FcBuilding> selectBuilding(Integer buildingNumber, String estateCode){
+        List<FcBuilding> list = new ArrayList<>();
+        FcBuilding fcBuilding;
+        for (Integer i = 0; i < buildingNumber; i++) {
+            fcBuilding = new FcBuilding();
+            fcBuilding.setEstateCode(estateCode);
+            fcBuilding.setBuildingCode(estateCode+"-"+i);
+            fcBuilding.setBuildingName(i+"幢");
+            fcBuildingMapper.insert(fcBuilding);
+            list.add(fcBuilding);
+        }
+        return list;
+    }
+
+    public Integer updateBuilding(FcBuilding fcBuilding){
+        return fcBuildingMapper.updateById(fcBuilding);
+    }
+
+    public Integer updateBatchBuilding(FcBuilding[] fcBuildings){
+        Integer count = 0;
+        for (FcBuilding fcBuilding : fcBuildings) {
+            count += fcBuildingMapper.updateById(fcBuilding);
+        }
+        return count;
     }
 }
